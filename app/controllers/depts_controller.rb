2f226e9e060401a6b8ba5df97eafb4192806d4c6
jbvoigt3 <jbvoigt3@gmail.com>
2018-09-05 14:49:18 -0600

    fleshed out the controller and was working on views.  Quitting.  Moving on to next project.

--- app/controllers/depts_controller.rb
+++ app/controllers/depts_controller.rb
@@ -1,26 +1,53 @@
 class DeptsController < ApplicationController
+  before_action :find_dept, only: [:show, :edit, :update, :destroy]
+
   def index
     @depts = Dept.all.order("name ASC")
   end
 
   def show
-    @depts = Dept.find(params[:id])
   end
 
   def new
-    @depts = Dept.new
+    @dept = Dept.new
+    render partial: "form"
   end
 
   def edit
-    @depts = Dept.find(params[:id]) 
+    render partial: "form"
   end
 
   def create
+    @dept = Dept.new(dept_params)
+
+    if @dept.save
+      redirect_to depts_path
+    else
+      render :new
+    end
   end
 
   def update
+    if @dept.update(dept_params)
+      redirect_to dept_path(@dept)
+    else
+      render :edit
+    end  
   end
 
   def destroy
+    @dept.destroy
+    redirect_to dept_path
   end
+
+  private
+  def dept_params
+    params.require(:dept).permit(:name, :loc)
+  end
+
+  def find_dept
+    @dept = Dept.find(params[:id])
+  end
+
+
 end
